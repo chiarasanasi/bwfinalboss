82d1bd09042c960a9e13b30cdcc47e2b4a392959
Marco Teodoro Mancuso <marco.teo.mancuso@gmail.com>
2025-06-27 11:59:52 +0200

    fix CloudinaryConfig

--- src/main/java/it/epicode/bwfinalboss/config/CloudinaryConfig.java
+++ src/main/java/it/epicode/bwfinalboss/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package it.epicode.bwfinalboss.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +10,22 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "tuo-cloud-name");
-        config.put("api_key", "tua-api-key");
-        config.put("api_secret", "tua-api-secret");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
-
